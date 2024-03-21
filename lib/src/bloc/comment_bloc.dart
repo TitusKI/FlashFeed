71900984ba92a7cdd699b87788515cd61e285b77
TitusKI <kidumazie@gmail.com>
2024-03-21 12:27:20 +0200

    StartWithCommentBloc

---
+++ lib/src/bloc/comment_bloc.dart
@@ -0,0 +1,16 @@
+import "package:rxdart/rxdart.dart";
+import "../models/item_models.dart";
+import '../resources/repository.dart';
+import "dart:async";
+
+class CommentsBloc {
+  final _commentFetcher = PublishSubject<int>();
+  final _commentOutput = BehaviorSubject<Map<int, Future<ItemModel?>>>();
+
+  // Stream
+  Stream<Map<int, Future<ItemModel?>>> get itemWithComments =>
+      _commentOutput.stream;
+
+  // Sink
+  Function(int) get fetchItemWithComments => _commentFetcher.sink.add;
+}
